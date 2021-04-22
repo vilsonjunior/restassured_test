d5a941e1bea88d833833d87b307c9c7b69f1488e
vilsonjunior <Gremio_1983>
2021-04-22 16:18:30 -0300

    ADD teste com get básico

--- src/test/java/RestAssuredTest.java
+++ src/test/java/RestAssuredTest.java
@@ -1,8 +1,21 @@
 import org.junit.Test;
 
-public class RestAssuredTest {
-    @Test
-    public void startRest() {
+import static io.restassured.RestAssured.when;
+import static org.hamcrest.CoreMatchers.is;
 
+public class RestAssuredTest {
+
+    private String urlStarWars = "https://swapi.dev/api/";
+
+    @Test
+    public void listaInfoLukeStarWars() {
+        when().
+                get(urlStarWars + "people/1/")
+        .then()
+                .statusCode(200)
+                .body("name", is("Luke Skywalker"))
+                .body("height", is("172"))
+                .body("gender", is("male"))
+                .body("birth_year", is("19BBY"));
     }
 }
